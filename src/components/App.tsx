1ff8a725533676cd8e4dbdaa0744bc723e7cd900
kyonsheeee <kyokoakatsuka38@gmail.com>
2023-07-08 20:37:26 +0900

    メイン部分

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,52 @@
+import React, { useRef } from "react";
+
+import { Todo } from "../hooks/Todo";
+import { TodoTitle } from "./TodoTitle";
+import { TodoAdd } from "./TodoAdd";
+import { TodoList } from "./TodoList";
+
 function App() {
-  return <p>Try to create TODO app!</p>
+  // カスタムフックから必要な変数を取得
+  const {
+    todoList,
+    toggleTodoListItemStatus,
+    addTodoListItem,
+    deleteTodoListItem,
+  } = useTodo();
+
+  const inputEl = useRef<HTMLTextAreaElement>(null);
+
+  const handleAddTodoListItem = () => {
+    if (inputEl.current?.value === "") {
+      return;
+    }
+    addTodoListItem(inputEl.current!.value);
+    inputEl.current!.value = "";
+  };
+
+  // 未完了リスト
+  const incompletedList = todoList.filter((todo: Todo) => !todo.done);
+  // 完了リスト
+  const completedList = todoList.filter((todo: Todo) => todo.done);
+
+  return (
+    <>
+      <TodoTitle title="TODO進捗管理" as="h1" />
+      <TodoAdd
+        buttonText="+ TODOを追加"
+        inputEl={inputEl}
+        handleAddTodoListItem={handleAddTodoListItem}
+      />
+      <TodoList
+        todoList={incompletedList}
+        toggleTodoListItemStatus={toggleTodoListItemStatus}
+      />
+      <TodoList
+        todoList={completedList}
+        toggleTodoListItemStatus={toggleTodoListItemStatus}
+      />
+    </>
+  );
 }
 
 export default App;
